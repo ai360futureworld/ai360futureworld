bc60f042215b8c98fdaad613a27498360530aeee
ai360futureworld <ai360futureworld@gmail.com>
2025-11-23 10:08:51 +0530

    Home Page Update

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -20,7 +20,7 @@ export default function Footer() {
           <Image
             src="/logo.png"
             alt="AI 360 Future"
-            width={120}
+            width={100}
             height={60}
             className="object-contain mb-4"
           />
